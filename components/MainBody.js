382065a7603c393913901f6d2946a61717a1feb0
Soysal Degirmenci <degirmenci@MacBook-Pro.local>
2023-03-06 13:28:52 -0800

    Move code around

--- components/MainBody.js
+++ components/MainBody.js
@@ -10,20 +10,6 @@ function MainBody() {
     const [userDefinedPrompt, setUserDefinedPrompt] = useState("");
     const [generationInProgress, setGenerationInProgress] = useState(false);
 
-    const promptTextSystem = `You are a sitcom script writer whose goal is to write scripts people love. Following the show and character descriptions and a title, write a script.
-    Sitcom description:
-    The show revolves around the everyday lives of four main characters, Jerry Seinfeld, George Costanza, Elaine Benes, and Cosmo Kramer, as they navigate their relationships, careers, and personal quirks in New York City.
-    The show often takes mundane situations and turns them into comedic gold, such as waiting for a table at a restaurant, going to a baseball game, or getting lost in a parking garage. The humor is often dry, witty, and observational, and the characters frequently get themselves into awkward and hilarious situations.
-    Each episode typically has multiple storylines that intersect in unexpected ways, and the show is known for its memorable catchphrases, recurring characters, and meta humor. Throughout the show's nine seasons, the characters experience a wide range of ups and downs, from breakups and job losses to failed business ventures and family drama.
-    Characters:
-    - Jerry Seinfeld: Jerry is a comedian who often uses his everyday experiences as material for his stand-up routines. He is depicted as witty, neurotic, and a bit self-centered.
-    - George Costanza: He is Jerry's best friend. George is depicted as insecure, dishonest, and frequently unemployed. He is often involved in schemes and lies to get what he wants, but they usually backfire. He is usually cheap with money, has a love hate relationship with his parents Estelle and Frank Costanza.
-    - Elaine Benes:  She is Jerry's ex-girlfriend. Elaine is depicted as intelligent, confident, and outspoken. She works as an editor and is known for her blunt, sarcastic sense of humor.
-    - Cosmo Kramer: He is Jerry's eccentric neighbor. Kramer is known for his wild hair, explosive entrances, and bizarre ideas. He often helps Jerry and his friends with their problems but is also known for causing chaos and mischief. He is quite unpredictable and always has the craziest ideas for businesses.
-    Guidelines for the script format:
-    - Annotate locations, gestures of the characters with brackets (example: [Int: Jerry’s apartment] [Int: Monk’s coffee shop]
-    `
-
     const sessionId = uuidv4();
 
     useEffect(() => {
@@ -63,13 +49,10 @@ function MainBody() {
 
     async function getCompletion() {
         const apiBody = {
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {role: 'system', content: promptTextSystem},
-                {role: 'user', content: userDefinedPrompt}],
-        };
+            promptText: userDefinedPrompt,
+        }
 
-        const response = await fetch('/api/openai', {
+        const response = await fetch('/api/generate', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
